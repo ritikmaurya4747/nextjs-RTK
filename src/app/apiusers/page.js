6602d110f81a43e265143400d806e5cfb593eb55
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-11-07 17:31:09 +0530

    api call working data getting in to UI

--- src/app/apiusers/page.js
+++ src/app/apiusers/page.js
@@ -1,7 +1,30 @@
+"use client";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchApiUsers } from "../redux/slice";
+
 export default function Page() {
+  const apiUserData = useSelector((data) => data.userData.userAPIData);
+  // console.log("API",apiUserData.userData.userAPIData);
+
+  const dispatch = useDispatch();
   return (
-    <div>
+    <div className="add-btn">
       <h1>User List from API</h1>
+      <button onClick={() => dispatch(fetchApiUsers())}>Load Users</button>
+      {
+        apiUserData &&
+        apiUserData.map((user) => (
+          <div key={user.id}>
+            <span>
+              <b>Name:-</b> {user.name}
+            </span><br />
+            <span>
+              <b>Email:-</b> {user.email}
+            </span>
+          </div>
+        )) ||
+        "Loading..."
+      }
     </div>
   );
 }
--- src/app/redux/slice.js
+++ src/app/redux/slice.js
@@ -1,13 +1,19 @@
-import { createSlice, nanoid , current, createAsyncThunk} from "@reduxjs/toolkit";
+import {
+  createSlice,
+  nanoid,
+  current,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
 
 const initialState = {
-  users: JSON.parse(localStorage.getItem('users')) || []
+  userAPIData:[],
+  users: JSON.parse(localStorage.getItem("users")) || [],
 };
 
-export const fetchApiUsers = createAsyncThunk("fetchApiUsers",async () =>{
-  const result = await fetch('https://jsonplaceholder.typicode.com/users');
+export const fetchApiUsers = createAsyncThunk("fetchApiUsers", async () => {
+  const result = await fetch("https://jsonplaceholder.typicode.com/users");
   return await result.json();
-})
+});
 
 const Slice = createSlice({
   name: "addUserSlice",
@@ -20,18 +26,25 @@ const Slice = createSlice({
       };
       state.users.push(data);
       let userData = JSON.stringify(current(state.users));
-      localStorage.setItem('users', userData);
+      localStorage.setItem("users", userData);
     },
-    removeUser:(state, action) => {
-     const data  = state.users.filter((item)=>{
-      return item.id!==action.payload
-     })
-     state.users = data
-     localStorage.setItem("users", JSON.stringify(data)); 
-    }
+    removeUser: (state, action) => {
+      const data = state.users.filter((item) => {
+        return item.id !== action.payload;
+      });
+      state.users = data;
+      localStorage.setItem("users", JSON.stringify(data));
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchApiUsers.fulfilled, (state, action) => {
+      (state.isloading = false), (state.userAPIData = action.payload);
+      // console.log("Reducer",action);
+    });
+
   },
 });
 
-export const { addUser , removeUser } = Slice.actions;
+export const { addUser, removeUser } = Slice.actions;
 
 export default Slice.reducer;
