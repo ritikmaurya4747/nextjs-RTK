838d754b6f2867c74248bf5408de55d9d759dcf9
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-11-07 14:40:58 +0530

    remove button working fine with the list

--- src/app/components/DisplayUsers.js
+++ src/app/components/DisplayUsers.js
@@ -1,22 +1,23 @@
-'use client'
-import { useSelector } from "react-redux";
-
+"use client";
+import { useDispatch, useSelector } from "react-redux";
+import {removeUser} from '../redux/slice'
 export default function DisplayUsers() {
-  const userData = useSelector((state) =>state.users);
-  // console.log(userData);
-  
+  const userData = useSelector((state) => state.users);
+const dispatch = useDispatch();
   return (
     <div className="main">
       <div className="show-user">
         <h1>User List</h1>
-        {
-          userData.map((list)=>(
+        <div className="list-center">
+          {userData.map((list) => (
             <div key={list.id} className="user-list">
-              Name : {list.name}
-              {list.id}
+              <span>
+                <b>Name:-</b> {list.name}
+              </span>
+              <button onClick={() =>dispatch(removeUser(list.id))}>Remove</button>
             </div>
-          ))
-        }
+          ))}
+        </div>
       </div>
     </div>
   );
