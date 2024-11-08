99c2b0d324a01560bd28170900880614e3b80be5
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-11-08 11:05:51 +0530

    adding () in nanoid

--- src/app/redux/todoslice.js
+++ src/app/redux/todoslice.js
@@ -11,7 +11,7 @@ const Slice = createSlice({
         addTodos:(state, action) => {
             // console.log(action);
             const data = {
-                id:nanoid,
+                id:nanoid(),
                 name:action.payload,
             }
             state.todos.push(data)
